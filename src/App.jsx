459bebddca04508ed1c7f30539e7cd47acd4187f
MustafaRush731 <rushmus226@gmail.com>
2024-06-05 15:56:52 -0500

    Carousel change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import './App.css'
 import SearchBar from './components/SearchBar'
 import Carousel from './components/Carousel'
+import SignIn from './components/SignIn'
+import CreateAccount from './components/CreateAccount'
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+
+
+const MainApp = () => {
   return (
     <main>
       <SearchBar />
       <Carousel />
     </main>
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <main>
+        <Routes>
+          <Route path="/" element={<MainApp />} />
+          <Route path="/my-account" element={<SignIn />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+        </Routes>
+      </main>
+    </Router>
   )
 }
 
 export default App
+
+
